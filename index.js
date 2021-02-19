ae5aad976d5e523fed6aab7d30bb4fdccb1ac6e1
Milan Cirkovic <67096753+subudei@users.noreply.github.com>
2021-02-19 22:25:09 +0100

    created function to create, find and delete user

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import express from "express";
-import bodyParser from "body-parser";
-import usersRoutes from "./routes/users.js";
+import bodyParser from "body-parser"; //in order to read HTTP POST data , we have to use "body-parser" node module. body-parser is a piece of express middleware that reads a form's input and stores it as a javascript object accessible through req.body
 
-const app = express();
+import usersRoutes from "./routes/users.js"; //
+
+const app = express(); // actualy the App !!!!!
 const PORT = 5000;
 
 app.use(bodyParser.json());
--- routes/users.js
+++ routes/users.js
@@ -1,28 +1,49 @@
 import express from "express";
+import { v4 as uuidv4 } from "uuid";
 
 const router = express.Router();
 
-const users = [
-  {
-    firstName: "John",
-    lastName: "Doe",
-    age: 18,
-  },
-  {
-    firstName: "Pablo",
-    lastName: "Aimar",
-    age: 21,
-  },
+let users = [
+  // {
+  //   firstName: "Claudio",
+  //   lastName: "Lopez",
+  //   number: 7,
+  // },
+  // {
+  //   firstName: "Pablo",
+  //   lastName: "Aimar",
+  //   number: 21,
+  // },
 ];
 
 router.get("/", (req, res) => {
-  console.log(users);
-
   res.send(users);
 });
 
-// router.post("/", (req,  res)=> {
+router.post("/", (req, res) => {
+  const user = req.body; // created usere in postman
 
-// })
-// install postmen  - puzirano na 23:11
+  users.push({ ...user, id: uuidv4() }); // adding user with unique Id (spread operator)
+
+  res.send(`User with the name ${user.firstName} added to the data base`);
+}); // adding users
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+
+  const foundUser = users.find((user) => user.id === id);
+
+  res.send(foundUser);
+});
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+
+  users = users.filter((user) => user.id !== id);
+
+  res.send(`User with the id ${id} deleted from the database`);
+});
+
 export default router;
+
+// paused at 44:00
